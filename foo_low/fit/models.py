dc44550abd37e5da81b0de97dbbf5f98844b62b7
acebrianm <alex.cebrianm@gmail.com>
2017-05-09 18:23:25 +0000

    Modified Tags->Tag, admin site now had crud of events, profile and tags. Ignore images folder. Models with return values.

--- foo_low/fit/models.py
+++ foo_low/fit/models.py
@@ -4,12 +4,18 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-class Tags(models.Model):
+class Tag(models.Model):
     tag_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return '%s' %self.tag_name
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_tag = models.ManyToManyField(Tags)
+    profile_tag = models.ManyToManyField(Tag)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return '%s' %self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -23,6 +29,9 @@ def save_user_profile(sender, instance, **kwargs):
 class Event(models.Model):
     event_name = models.CharField(max_length=30)
     description = models.TextField(max_length=140)
-    image = models.ImageField(upload_to='images/', height_field=400, width_field=400)
+    image = models.ImageField(upload_to='images/')
     date = models.DateField()
-    event_tag = models.ManyToManyField(Tags)
+    event_tag = models.ManyToManyField(Tag)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return '%s' %self.event_name
