7cdefaf311970c38367cd0913412139ce9be925c
acebrianm <alex.cebrianm@gmail.com>
2017-05-09 16:53:30 +0000

    Database models created.

--- foo_low/fit/models.py
+++ foo_low/fit/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=50)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_tag = models.ManyToManyField(Tags)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=30)
+    description = models.TextField(max_length=140)
+    image = models.ImageField(upload_to='images/', height_field=400, width_field=400)
+    date = models.DateField()
+    event_tag = models.ManyToManyField(Tags)
--- foo_low/foo_low/settings.py
+++ foo_low/foo_low/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fit',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'foo_low.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'foo_low',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
