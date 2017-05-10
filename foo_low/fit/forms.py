a2f7afde3c6ae8f55e1583b8bba38709bab7b0bb
acebrianm <alex.cebrianm@gmail.com>
2017-05-10 08:34:05 +0000

    Finished CRUD for tags

--- foo_low/fit/forms.py
+++ foo_low/fit/forms.py
@@ -5,9 +5,13 @@ from .models import Profile, Tag
 class TagForm(ModelForm):
     profile_tag = forms.ModelMultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
-        queryset=Tag.objects.all())
+        queryset=Tag.objects.all().filter(is_active=True))
 
     class Meta:
         model = Profile
         fields = ['profile_tag']
         
+class TagAdminForm(ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['tag_name']
