0c67b9b540035999a3aab041d846ac9099997ad0
raul <raul619@icloud.com>
2017-05-10 07:38:41 +0000

    List view added
    It differentiates from super user to normal user and displays different information for them.
    Normal users can see their desired events according to their tags.
    Links for tags works and searches for all the events with that specific tag.
    List view displayed

--- foo_low/fit/urls.py
+++ foo_low/fit/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^list/$', views.list, name='list'),
+    url(r'^list/(?P<tag>[0-9a-zA-Z_]+)/$', views.list, name="list"),
 ]
--- foo_low/fit/views.py
+++ foo_low/fit/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 
+from datetime import datetime
+
+# Models
+from .models import Profile, Tag, Event
+
 
 # Logout
 from django.contrib.auth import logout
@@ -10,9 +15,35 @@ from django.contrib.auth import logout
 def index(request):
 
     # Template render
+    tag_list = Tag.objects.all()
     template = loader.get_template('fit/index.html')
     context = {
-            'tag_list': ["Sports", "culture"]
+            'tag_list': tag_list
     }
     return HttpResponse(template.render(context, request))
 
+def list(request, tag=None):
+
+    tag_list = Tag.objects.all()
+
+    if request.user.is_superuser:
+        if tag:
+            events = Event.objects.filter(event_tag__tag_name=tag).distinct()
+        else:
+            events = Event.objects.all().distinct()
+    else:
+        if tag:
+            events = Event.objects.filter(event_tag__tag_name=tag).filter(date__gte=datetime.now()).distinct()
+        else:
+            user_tags = Profile.objects.get(id=request.user.id).profile_tag.all()
+            events = Event.objects.filter(event_tag__in=user_tags).filter(is_active=True).filter(date__gte=datetime.now()).distinct() 
+    images = []
+    for event in events:
+        images.append(event.image.name.split('/')[4])
+        
+    template = loader.get_template("fit/list.html")
+    context = {
+        'tag_list': tag_list, 
+        'events' : zip(events, images)
+    }
+    return HttpResponse(template.render(context, request))
